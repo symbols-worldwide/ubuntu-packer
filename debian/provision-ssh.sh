231515e73bb26052ec1e7b39360a939a7e1e8d89
Dan Yaxley <dan@widgit.com>
2023-02-22 17:40:23 +0000

    Provision ssh for vagrant user and don't delete its home dir because we still need it, idiot

--- debian/provision-ssh.sh
+++ debian/provision-ssh.sh
@@ -16,7 +16,6 @@ echo "Please attach Docker disk to VM."
 echo "********************************"
 echo "Root password set to ${PASS}"
 echo "********************************"
-rm -rf /home/vagrant
 rm -rf /var/tmp/*
 rm -rf /root/*
 
@@ -25,3 +24,9 @@ echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7zLHVDpjrcOB2oCd1nMGGZcorobosBmANGo
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNXnouMxaGRpBdrUxa+T71w3XxudUYzP42A3CzjLTUv sysadmin@cubbington' >> /root/.ssh/authorized_keys
 chown root:root /root/.ssh/*
 chmod 600 /root/.ssh/*
+
+mkdir -p /home/vagrant/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7zLHVDpjrcOB2oCd1nMGGZcorobosBmANGo3mxwNgzk/ObvSU4Ye9crwVCzIp/OozYS95giNkpC7Kgv9llMCCChOU6/b8MXxblEdUrRrqt5CuXVe5XiM0imRa6jVH6SBymJqL17tcBpi5jVRiwEfM0tw2Ehzg0hh93APRis0bviu5HvvRT3FKg+mczNqdFHJA9oAnP7tYxlELXueyJv3qXEFiVvPppFyMv1vwIh8Sb6SZTnr95KAZjqYKlHig/aQctwp2WYmxVSSd05tDSuvlyrdZ7io86EyczYS6BBpv8gVQRdXB7of+R/XdRTHBGfIiZjYU6AHrePXZIAk51XbX rails-upload-2.pem' > /home/vagrant/.ssh/authorized_keys
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNXnouMxaGRpBdrUxa+T71w3XxudUYzP42A3CzjLTUv sysadmin@cubbington' >> /home/vagrant/.ssh/authorized_keys
+chown vagrant:vagrant /home/vagrant/.ssh/*
+chmod 600 /home/vagrant/.ssh/*
